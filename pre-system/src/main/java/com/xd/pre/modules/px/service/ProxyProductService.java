901771d9dc69c3c8e144fec107a4361373adfb4e
xuhao <412900691@qq.com>
2022-10-18 00:20:46 +0800

    匹配订单号有问题。掉单

--- pre-system/src/main/java/com/xd/pre/modules/px/service/ProxyProductService.java
+++ pre-system/src/main/java/com/xd/pre/modules/px/service/ProxyProductService.java
@@ -140,7 +140,10 @@ public class ProxyProductService {
                 JSONObject parseObject = JSON.parseObject(JSON.toJSONString(o));
                 String port = parseObject.get("port").toString();
                 String ip = parseObject.get("ip").toString();
-                DateTime validTime = DateUtil.parseDateTime(parseObject.getString("validTime"));
+                DateTime validTime = DateUtil.offsetSecond(new Date(), 270);
+                if (parseObject.containsKey("validTime")) {
+                    validTime = DateUtil.parseDateTime(parseObject.getString("validTime"));
+                }
                 JdProxyIpPort jdProxyIpPort = new JdProxyIpPort().builder().agentAddress(producUrl).ip(ip)
                         .port(port).createTime(new Date()).isUse(0).expirationTime(validTime).build();
                 jdProxyIpPortMapper.insert(jdProxyIpPort);
