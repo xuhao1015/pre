4a84e69d7651a683d9d81a5841475410b2a9f874
xuhao <412900691@qq.com>
2022-10-20 19:42:06 +0800

    修改时区不对导致时间不对

--- pre-system/src/main/java/com/xd/pre/modules/security/config/LocalDateTimeSerializerConfig.java
+++ pre-system/src/main/java/com/xd/pre/modules/security/config/LocalDateTimeSerializerConfig.java
@@ -7,11 +7,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.jackson.JsonComponent;
 import org.springframework.context.annotation.Bean;
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 
 /**
  * @description: 日期时间全局格式化
@@ -30,13 +30,14 @@ public class LocalDateTimeSerializerConfig {
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilder() {
         return builder -> {
+            TimeZone tz = TimeZone.getTimeZone("UTC");//获取时区
             DateFormat df = new SimpleDateFormat(pattern);//设置格式化模板
+            df.setTimeZone(tz);
             builder.failOnEmptyBeans(false)
                     .failOnUnknownProperties(false)
                     .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                     .dateFormat(df);
-        };
-    }
+        }; }
 
     /**
      * LocalDate 类型全局时间格式化
