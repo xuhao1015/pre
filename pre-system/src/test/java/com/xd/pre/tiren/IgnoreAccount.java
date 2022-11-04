3c159d48d16ab94e957644ab49b94ebf00b409a7
[xuhao1015] <[412900691@qq.com]>
2022-11-04 18:29:00 +0800

    添加检查数据，从新匹配数据

---
+++ pre-system/src/test/java/com/xd/pre/tiren/IgnoreAccount.java
@@ -0,0 +1,38 @@
+package com.xd.pre.tiren;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.db.Entity;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+public class IgnoreAccount {
+
+    public static void main(String[] args) throws Exception {
+        List<Entity> query1 = FindOrder.db.query("SELECT * FROM pre.douyin_app_ck WHERE is_enable = '1' AND fail_reason LIKE '%当前下单人数过多%'");
+        List<Entity> query2 = FindOrder.db.query("SELECT * FROM pre.douyin_app_ck WHERE is_enable = '1' AND fail_reason LIKE '%设备存在异常%'");
+        List<Entity> query3 = FindOrder.db.query("SELECT * FROM pre.douyin_app_ck WHERE is_enable = '1' AND fail_reason LIKE '%挤爆了%'");
+        List<Entity> query4 = FindOrder.db.query("SELECT * FROM pre.douyin_app_ck WHERE is_enable = '1' AND fail_reason LIKE '%当前订单存在交易风险%'");
+        ArrayList<Entity> query = new ArrayList<>();
+        query.addAll(query1);
+        query.addAll(query2);
+        query.addAll(query3);
+        query.addAll(query4);
+        if (CollUtil.isEmpty(query)) {
+            return;
+        }
+        try {
+            for (Entity entity : query) {
+                String uid = entity.getStr("uid");
+                BlackService.jedis.del("抖音和设备号关联:" + uid);
+                log.info("删除成功关联成功:{},", uid);
+                FindOrder.db.execute("update douyin_app_ck  set is_enable =0 ,fail_reason  = '' where id = ?", entity.getInt("id"));
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+
+    }
+}
