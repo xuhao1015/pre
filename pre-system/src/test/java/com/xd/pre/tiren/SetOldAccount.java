3306249cd64c3508ab69124983bf1443975773aa
xuhao <412900691@qq.com>
2022-11-06 03:33:36 +0800

    设置是否是老账号

---
+++ pre-system/src/test/java/com/xd/pre/tiren/SetOldAccount.java
@@ -0,0 +1,22 @@
+package com.xd.pre.tiren;
+
+import cn.hutool.db.Entity;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class SetOldAccount {
+    public static void main(String[] args) throws Exception {
+        List<Entity> query = FindOrder.db.query("select pt_pin ,count(1) as count from jd_order_pt   where create_time < '2022-11-05 22:00:00'  and    pay_success_time is not null   GROUP BY pt_pin");
+        List<Entity> query1 = FindOrder.db.query("select pt_pin ,count(1) as count from jd_order_pt_11_04   where create_time < '2022-11-05 22:00:00'  and    pay_success_time is not null   GROUP BY pt_pin");
+        query.addAll(query1);
+        Set<String> pt_pins = query1.stream().map(it -> it.getStr("pt_pin")).collect(Collectors.toSet());
+        for (String pt_pin : pt_pins) {
+            FindOrder.db.execute("update douyin_app_ck set is_old =1 where uid =?", pt_pin);
+            log.info("设置老号成功：{}", pt_pin);
+        }
+    }
+}
