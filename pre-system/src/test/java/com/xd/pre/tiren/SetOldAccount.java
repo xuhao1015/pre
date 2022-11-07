716f8db0ae08dc0594d37c43c9e889c8758cf3d0
xuhao <412900691@qq.com>
2022-11-07 14:59:33 +0800

    添加100了设备号的关系。新加了点账号

--- pre-system/src/test/java/com/xd/pre/tiren/SetOldAccount.java
+++ pre-system/src/test/java/com/xd/pre/tiren/SetOldAccount.java
@@ -10,14 +10,11 @@ import java.util.stream.Collectors;
 @Slf4j
 public class SetOldAccount {
     public static void main(String[] args) throws Exception {
-        List<Entity> query = FindOrder.db.query("select pt_pin ,count(1) as count from jd_order_pt   where create_time < '2022-11-05 22:00:00'  and    pay_success_time is not null   GROUP BY pt_pin");
-        List<Entity> query1 = FindOrder.db.query("select pt_pin ,count(1) as count from jd_order_pt_11_04   where create_time < '2022-11-05 22:00:00'  and    pay_success_time is not null   GROUP BY pt_pin");
+        List<Entity> query = FindOrder.db.query("select pt_pin ,count(1) as count from jd_order_pt   where create_time < '2022-11-06 22:00:00'  and    pay_success_time is not null   GROUP BY pt_pin");
+        List<Entity> query1 = FindOrder.db.query("select pt_pin ,count(1) as count from jd_order_pt_11_04   where create_time < '2022-11-06 22:00:00'  and    pay_success_time is not null   GROUP BY pt_pin");
         query.addAll(query1);
         Set<String> pt_pins = query.stream().map(it -> it.getStr("pt_pin")).collect(Collectors.toSet());
         for (String pt_pin : pt_pins) {
-            if(pt_pin.equals("13623251438975")){
-                System.out.println("xxx");
-            }
             FindOrder.db.execute("update douyin_app_ck set is_old =1 where uid =?", pt_pin);
             log.info("设置老号成功：{}", pt_pin);
         }
