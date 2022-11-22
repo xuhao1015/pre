47fef6be65982dfb2948768b5140a690dfe382df
xuhao <412900691@qq.com>
2022-11-23 01:33:20 +0800

    查询额滴

---
+++ pre-system/src/test/java/com/xd/pre/tiren/查询额度.java
@@ -0,0 +1,23 @@
+package com.xd.pre.tiren;
+
+import cn.hutool.db.Entity;
+import com.alibaba.fastjson.JSON;
+import com.xd.pre.douyinnew.TestResoData;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+public class 查询额度 {
+    public static void main(String[] args) throws Exception {
+        List<Entity> query = FindOrder.db.query("select * from douyin_app_ck where is_enable =1 ");
+        Integer ed = 0;
+        for (Entity entity : query) {
+            String uid = TestResoData.jedis.get("抖音各个账号剩余额度:" + entity.getStr("uid"));
+            Integer balance = JSON.parseObject(uid).getInteger("balance");
+            ed = balance + ed;
+        }
+        System.err.println(ed);
+        log.info("剩余额度======================"+ed);
+    }
+}
