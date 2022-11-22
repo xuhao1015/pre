7dae93438e7d22481fd0add2cfa93998194ece63
xuhao <412900691@qq.com>
2022-11-23 01:35:35 +0800

    查询额滴

--- pre-system/src/test/java/com/xd/pre/tiren/查询额度.java
+++ pre-system/src/test/java/com/xd/pre/tiren/查询额度.java
@@ -9,15 +9,20 @@ import java.util.List;
 
 @Slf4j
 public class 查询额度 {
+
     public static void main(String[] args) throws Exception {
-        List<Entity> query = FindOrder.db.query("select * from douyin_app_ck where is_enable =1 ");
-        Integer ed = 0;
-        for (Entity entity : query) {
-            String uid = TestResoData.jedis.get("抖音各个账号剩余额度:" + entity.getStr("uid"));
-            Integer balance = JSON.parseObject(uid).getInteger("balance");
-            ed = balance + ed;
+        for(int i =0;i<100000;i++){
+            List<Entity> query = FindOrder.db.query("select * from douyin_app_ck where is_enable =1 ");
+            Integer ed = 0;
+            for (Entity entity : query) {
+                String uid = TestResoData.jedis.get("抖音各个账号剩余额度:" + entity.getStr("uid"));
+                Integer balance = JSON.parseObject(uid).getInteger("balance");
+                ed = balance + ed;
+            }
+            System.err.println(ed);
+            log.info("剩余额度======================"+ed);
+            Thread.sleep(2000);
         }
-        System.err.println(ed);
-        log.info("剩余额度======================"+ed);
+
     }
 }
