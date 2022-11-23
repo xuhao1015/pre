065e233c4fbe383c6f3a67bee75d434fbe1624ff
xuhao <412900691@qq.com>
2022-11-23 19:52:37 +0800

    让多账号继续算法更炫

--- pre-system/src/test/java/com/xd/pre/TestDemo.java
+++ pre-system/src/test/java/com/xd/pre/TestDemo.java
@@ -1,15 +1,12 @@
 package com.xd.pre;
 
-import cn.hutool.core.io.FileUtil;
 import com.xd.pre.common.utils.px.PreUtils;
-
-import java.util.List;
 
 public class TestDemo {
 
 
-    public static void main(String[] args) throws Exception{
-        while (true){
+    public static void main(String[] args) throws Exception {
+        while (true) {
             Integer i = PreUtils.randomCommon(0, 2 - 1, 1)[0];
             System.out.println(i);
             Thread.sleep(1000);
